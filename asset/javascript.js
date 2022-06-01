3a99f50c5e5e8e1a2a036ec95add25ae201cbcb3
unknown <mcsalazar122@gmail.com>
2022-05-31 23:10:43 -0500

    added time block colors

--- asset/javascript.js
+++ asset/javascript.js
@@ -1,64 +1,91 @@
 var setDate = document.querySelector("#currentDay");
 var currentDate = moment();
 setDate.textContent = currentDate.format("dddd, MMM Do");
+//var tasks = [];
 
 // set rows with color based on hour for each row
+// find date and change to number
+$(".time-block").each(function() {
+    var timeBlockHour = parseInt($(this).attr("id"))
+    
+   // $(".time-block").removeClass(".past")
+   var currentTime = moment().format ("HH")
 
-// find date first 
-
-//var currentTime = moment().format ("HH:mm");
-
-var setTime = document.querySelector(".hour p")
-//works in console log
-function FindTime (setTime) {
-
- //$(setTime).removeClass("present future");   
-// should but does not work
-if (currentDate.isBefore(setTime)) {
-    $(".hour").addClass(".past")
-   // $(parent).addClass(".past")
-} else
-if (currentDate.isAfter(setTime)){
-    $(".hour").addClass("future")
-} else
-if (currentDate()==(setTime)){
-    $(".hour").addClass("present")
-}
-
-};
-
-//end should but does not work
+    if (currentTime > timeBlockHour) {
+        $(".time-block").addClass("past");
+    }
+    else if (currentTime < timeBlockHour) {
+        $("time-block").addClass(".future");
+    }
+    else if (currentTime === timeBlockHour) {
+        $(".time-block").addClass(".present");
+    }
+}); 
+//end time block color
 
 
 //start adding a task to the page
-//set up input on click in <textarea>
-var createTask = function(taskText) {
+//set up input on click in <textarea> input works, need to save
+
+
+
+
+//create save button with icon
+$(".saveBtn").click(function(saveTasks){
+
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};    
+///problems
+var createTask = function(taskText, taskHour) {
     // create tasks in time blocks
 var taskAdd = $("<textarea>")
-    .addClass("tasks")
-    .text(taskText);
+    .addClass("task");
+var taskH = $("p")
+    .text(taskHour);
+}   
     //append task
-    taskAdd.append(taskText);
+    taskAdd.append(taskAdd, taskH);
+});   
 
-    //     //check time
-    //    // CheckTime(taskText)
-     $("#list-" + taskAdd).append(taskText);
-};
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+}
+//     //     //check time
+//     //    // CheckTime(taskText)
+//      $("#list-" + taskAdd).append();
+// };
+
+// var taskDataObj = {
+// id: 1,
+// name: taskText
+
+// }
+// var taskText = $("task").val();
 
 
+$("#dayTask").on("click", "textarea", function() {
+    var status = $(this)
+    .text()
+    .trim();
+//get id info (later for persist)
+    var status = $(this)
+    .closest(".hour")
+    .att("id")
+    .replace("list-","");
 
+    var index = $(this)
+        .closest(".row")
+        .index();
 
-// $("#dayTask").on("click", "textarea", function() {
-
-//     var text = $(this)
-//     .text()
-//     .trim();
-// //get id info (later for persist)
-//     var status = $(this)
-//     .closest(".task")
-//     .att("id")
-//     .replace("list-","");
-
+    var textInput = $("textarea>")
+    .addClass("tasks")
+    .val(text);
+    $(this).replaceWith(textInput)
+    tasks[status][index].text = text;
+    saveTasks();
+});
     //module 5.1.6 if needed
 // tasks[status][index].text = text;
 // saveTasks();
@@ -72,7 +99,7 @@ var taskAdd = $("<textarea>")
 
     // var textInput = $("<textarea>")
     // .addClass("task")
-    // .val(text);
+    // .val("hour");
     // $(this).replaceWith(textInput);
     // textInput.trigger("focus");
 
@@ -99,27 +126,3 @@ var taskAdd = $("<textarea>")
 
 
 
-// // setTime.textContent = currentTime.format("HH:mm")
-
-// //var pastTime = moment().
-
-// $("time datetime").p().datetime($("div").data("p").datetime);
-
-// $(doucment).children().filter(function(){
-//     return $(this).data("datetime"):
-
-// })
-
-// var checkTime = function(timeEl) {
-//     var findTime = $(timeEl)
-//     .find("time datetime")
-//     .text()
-//     .trim();
-    
-//     //var time = moment(time, "L").set("hour", currentTime)
-
-   // 
-
-
-// };
-// // 
