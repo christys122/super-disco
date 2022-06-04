c44e6db0b72f03b53966cc65a3430712ef853820
unknown <mcsalazar122@gmail.com>
2022-06-04 15:03:22 -0500

    read me

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 ## Work Day Scheduler
 
+This project is designed to help the user plan their work day.   This will help the user easily see what has past and what tasks they have coming up.
+
+## Features
+
+When you open the page you see the current day displayed at the top. This was acheived using moment.js.
+
+The page is divided into rows and columns using Bootstrap.  Depending on the time of day, the current hour is white, the past is gray and the future is green. This was also acheived utiliizing moment.js jquery and javascript.
+
+Once an entry is made and the save button clicked the data is stored in localStorage to be retreived upon reopening of the page.
+
+
+
+## Issues/Bugs
+
+I ran into several issues working on this page.  I was able to identify and correct most of them by using the console log and debugger to pinpoint and correct the errors.  I also received help from my peers and by searching my resources. Some bugs currently reamin and are in progress
+
+## Lessons Learned
+
+This was a very eye-opening project.  I struggled in the begining with the new functionality.  The more time I put into the project reworking the code and researching resolutions, the more clear it became.  The code is now easier to understand which will help when make future enhancements and debugging.  I can tell I am still learning, but I can acknowlege in myself how far I have come giving me something to be proud of.
--- asset/javascript.js
+++ asset/javascript.js
@@ -9,25 +9,35 @@ var taskHour = document.querySelector("timeblock");
 
 $(document).ready(function() {
 $(".saveBtn").click(function(){
-    var task = $.trim($("textarea").val());
+   // var task = $.trim($("textarea").sibling().val());
+   var task = $(this).siblings("#dayTask").children("textarea").val()
+    var time = parseInt($(this).parent().attr("id"))
     if (task !="") {
-    alert(task);
+    alert(task + " " + time);
     }
 
-//var text = $(task).siblings(".daytask").val();
-    var time = parseInt($(this).attr("id"))
-    
-    var taskObj = {task, time}
-
 //save to local 
-    localStorage.setItem("task", JSON.stringify(taskObj));
+    localStorage.setItem(time, JSON.stringify(task));
+});
 });
 
-var loadTasks = function() {
-    taskObj = JSON.parse(localStorage.getItem("task"));
-   
-    } 
+function loadTasks() {
+    var task = localStorage.getItem(task)
+    var time = localStorage.getItem(time)
+    if (time) {
+    
+    document.getElementById("daytask").value = task;
+    document.getElementById("").value = time;
+    textareaElement.taskarea = task
+    divElement.setAttribute("#" + time);
+   // task = JSON.parse(localStorage.getItem("time"));
 
+     loadTasks();
+}
+else { 
+    console.log("no value");
+}
+}
 // find date and change to number
 
 $(".time-block").each(function() {
@@ -54,65 +64,33 @@ $(".time-block").each(function() {
      
   };
 });
+
+      
+// enable draggable/sortable feature on task list elements
+
+$(".time-block").sortable({
+    connectWith: $("#daytask", ),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event, textarea) {
+    console.log(textarea);
+  },
+  deactivate: function(event, textarea) {
+    console.log(textarea);
+  },
+  over: function(event) {
+    console.log(event);
+  },
+  out: function(event) {
+    console.log(event);
+  },
+  update: function() {
+    var tempArr = [];
+}
 });
+    // enable dragging 
 
-
-    
-// });        
-   
-//   // enable draggable/sortable feature on list-group elements
-// $(".row .time-block").sortable({
-//     // enable dragging across lists
-//     connectWith: $(".row .time-block"),
-//     scroll: false,
-//     tolerance: "pointer",
-//     helper: "clone",
-//     activate: function(event, textarea) {
-//       console.log(textarea);
-//     },
-//     deactivate: function(event, textare) {
-//       console.log(textarea);
-//     },
-//     over: function(event) {
-//       console.log(event);
-//     },
-//     out: function(event) {
-//       console.log(event);
-//     },
-//     update: function() {
-//       var tempArr = [];
-  
-//       // loop over current set of children in sortable list
-//       $(this)
-//         .children()
-//         .each(function() {
-//           // save values in temp array
-//           tempArr.push({
-//             text: $(this)
-//               .find("textarea")
-//               .text()
-//               .trim(),
-//             date: $(this)
-//               .find("p")
-//               .text()
-//               .trim()
-//           });
-//         });
-  
-//       // trim down list's ID to match object property
-//       var arrName = $(this)
-//         .attr("id")
-//         .replace("list-", "");
-  
-//       // update array on tasks object and save
-//       tasks[arrName] = tempArr;
-//       saveTasks();
-//     },
-//     // stop: function(event) {
-//     //   $(this).removeClass("dropover");
-//     // }
-//   });
-    
     
 
 
